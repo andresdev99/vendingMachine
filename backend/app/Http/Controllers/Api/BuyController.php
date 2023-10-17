8970b9510bc38c7df03a63dad619add86d8e8c9b
andres <andresfelix951@gmail.com>
2023-10-17 09:27:52 -0500

    adding last comments

--- backend/app/Http/Controllers/Api/BuyController.php
+++ backend/app/Http/Controllers/Api/BuyController.php
@@ -9,12 +9,19 @@ use Illuminate\Http\Request;
 
 class BuyController extends Controller
 {
+    /**
+     * Handle a request to buy an item.
+     */
     public function buyItem(BuyRequest $request){
         $data            = $request->validated();
         $out = [];
+
+        // Check item availability
         if ($data["item"]["availability"] > 0) {
             if ($data["item"]["cost"] <= $data["money"]) {
+                // Capitalize item names
                 $out["message"]  = "Enjoy your " . ucfirst($data["item"]["name"]);
+                // Substract 1
                 $out["response"] = $data["item"]["availability"] - 1;
                 $out["change"]   = number_format($data["money"] - $data["item"]["cost"], 2);
             } else {
@@ -27,14 +34,22 @@ class BuyController extends Controller
         return response($out);
     }
 
+
+    /**
+     * Handle a request to return money.
+     */
     public function returnMoney(ReturnMoneyRequest $request)
     {
         $data             = $request->validated();
         $moneyHistory     = $data["moneyHistory"];
+        // Format to only have 2 number after point
         $moneyHistorySum  = number_format(array_sum($data["moneyHistory"]), 2);
+        // Format to only have 2 number after point
         $totalMoney       = number_format($data["totalMoney"], 2);
 
+        // Check if the total count has the same sum which is in the input money
         if ($totalMoney > 0 && $totalMoney == $moneyHistorySum) {
+            // Split every response by line
             $out["response"] = implode(",\n", $moneyHistory);
             $out["message"]  = "Returned Money";
         } else {
--- frontend/src/axios-client.js
+++ frontend/src/axios-client.js
@@ -1,29 +1,37 @@
 import axios from "axios";
 
+// Create an instance of Axios with a base URL
 const axiosClient = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL
 })
 
-
+// Request interceptor
 axiosClient.interceptors.request.use((config) => {
+    // Modify the request config if needed
     return config;
 })
 
-
+// Response interceptor
 axiosClient.interceptors.response.use((response) => {
+    // Handle successful responses
     return response;
 }, (error) => {
     try {
         const { response } = error;
 
         if (response === 401) {
-            // Do something when error
+            // Handle specific error code (401 Unauthorized)
+            // You can add custom logic here
         } else {
-            // Other statuses
+            // Handle other HTTP statuses
+            // You can define additional error handling logic
         }
     } catch (error) {
+        // Handle unexpected errors
         console.log(error);
     }
+
+    // Re-throw the error to propagate it further
     throw error;
 })
 export default axiosClient;
